db2a5d7db54ad7816108c4cba6ca48aa9d568d49
Vika-star <72336972+Vika-star@users.noreply.github.com>
2021-12-01 11:32:40 +0300

    changing sections

--- #src/js/script.js
+++ #src/js/script.js
@@ -23,22 +23,23 @@ btnSend.addEventListener("click", function () {
   }
 });
 
-
 function IsValidInput(input) {
   return input.validity.valid;
 }
 
-const mapInfo = new Map();
-mapInfo.set(document.getElementById("bannerContent"), { color: "white" });
-mapInfo.set(document.getElementById("aboutContent"), { color: "black" });
-mapInfo.set(document.getElementById("servicesContent"), { color: "black" });
 
-window.addEventListener('scroll', function () {
 
-  for (const key of mapInfo.keys()) {
-    const domRect = key.getBoundingClientRect();
-    if (domRect.top >= 0 && domRect.bottom >= 0) {
-      console.log(key);
-    }
-  }
-});
+// const mapInfo = new Map();
+// mapInfo.set(document.getElementById("bannerContent"), { color: "white" });
+// mapInfo.set(document.getElementById("aboutContent"), { color: "black" });
+// mapInfo.set(document.getElementById("servicesContent"), { color: "black" });
+
+// window.addEventListener('scroll', function () {
+
+//   for (const key of mapInfo.keys()) {
+//     const domRect = key.getBoundingClientRect();
+//     if (domRect.top >= 0 && domRect.bottom >= 0) {
+//       console.log(key);
+//     }
+//   }
+// });
