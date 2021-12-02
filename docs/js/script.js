486597fe49abaa59f594af4217a24261e78b54c5
Vika-star <72336972+Vika-star@users.noreply.github.com>
2021-12-02 15:12:15 +0300

    scroll added

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,59 @@
+const homeSection = document.getElementById('bannerSection');
+let homeSectionTop = homeSection.offsetTop;
+
+const aboutSection = document.getElementById('aboutSection');
+let aboutSectionTop = aboutSection.offsetTop - 310;
+
+const servicesSection = document.getElementById('servicesSection');
+let servicesSectionTop = servicesSection.offsetTop - 400;
+
+const contactSection = document.getElementById('contactSection');
+let contactSectionTop = contactSection.offsetTop - 300;
+
+const scrollLines = document.querySelectorAll('.scroll__wrapper-item');
+
+function clearLines() {
+  scrollLines.forEach(item => {
+    item.classList.remove('activeLine');
+  })
+}
+
+function activateColor(scrollTop) {
+
+  if(scrollTop < aboutSectionTop || scrollTop > contactSectionTop){
+    document.querySelector('.scroll__wrapper').classList.add('white');
+  }
+  else {
+    document.querySelector('.scroll__wrapper').classList.remove('white');
+  }
+}
+
+function activateLines(scrollTop) {
+  if(scrollTop >= 0 && scrollTop < aboutSectionTop){
+    clearLines();
+    document.querySelector('.scroll__wrapper-item.first').classList.add('activeLine');
+  }
+  else if(scrollTop< servicesSectionTop && scrollTop> aboutSectionTop){
+    clearLines();
+    document.querySelector('.scroll__wrapper-item.second').classList.add('activeLine');
+  }
+  else if (scrollTop < contactSectionTop && scrollTop > servicesSectionTop) {
+    clearLines();
+    document.querySelector('.scroll__wrapper-item.third').classList.add('activeLine');
+  } 
+  else {
+    clearLines();
+    document.querySelector('.scroll__wrapper-item.fourth').classList.add('activeLine');
+  }
+}
+
+document.addEventListener("scroll", function () {
+  let scrollTop = document.documentElement.scrollTop;
+  activateLines(scrollTop);
+  activateColor(scrollTop);
+})
+
+
 
 const iconMenu = document.querySelector('.menu__icon');
 if (iconMenu) {
@@ -12,7 +68,7 @@ for (const menuLink of menuLinks) {
 function closeMobileMenu() {
   const bodyMenu = document.querySelector('.menu__body');
   const muneLogo = document.querySelector('.header__logo-img');
-  
+
   document.body.classList.toggle('_lock');
   iconMenu.classList.toggle('_active');
   bodyMenu.classList.toggle('_active');
@@ -44,40 +100,40 @@ function IsValidInput(input) {
 }
 
 
-const targetsProperties = new Map();
+// const targetsProperties = new Map();
 
-targetsProperties.set("bannerNavigation", { color: "white", name: "Banner" });
-targetsProperties.set("aboutUsNavigation", { color: "black", name: "About" });
-targetsProperties.set("servicesNavigation", { color: "black", name: "Services" });
-targetsProperties.set("contactNavigation", { color: "white", name: "Contact" });
+// targetsProperties.set("bannerNavigation", { color: "white", name: "Banner" });
+// targetsProperties.set("aboutUsNavigation", { color: "black", name: "About" });
+// targetsProperties.set("servicesNavigation", { color: "black", name: "Services" });
+// targetsProperties.set("contactNavigation", { color: "white", name: "Contact" });
 
 
-const navItems = document.getElementsByClassName('nav-item');
+// const navItems = document.getElementsByClassName('nav-item');
 
-const spans = Array.from(document.getElementsByClassName('navigationName'));
-let links = Array.from(document.getElementsByClassName('scroll-navigation__li'));
+// const spans = Array.from(document.getElementsByClassName('navigationName'));
+// let links = Array.from(document.getElementsByClassName('scroll-navigation__li'));
 
-$(window).on('activate.bs.scrollspy', function (e, obj) {
+// $(window).on('activate.bs.scrollspy', function (e, obj) {
 
-  let targetProperties = targetsProperties.get(e.target.id.toString());
+//   let targetProperties = targetsProperties.get(e.target.id.toString());
 
-  setName(targetProperties.name, e.target);
-  setColor(targetProperties.color)
-});
+//   setName(targetProperties.name, e.target);
+//   setColor(targetProperties.color)
+// });
 
-function setName(name, target) {
-  spans.map(item => {
-    let span = item;
-    span.innerText = "";
-    return span;
-  });
-  target.querySelector('span').innerText = name;
-}
+// function setName(name, target) {
+//   spans.map(item => {
+//     let span = item;
+//     span.innerText = "";
+//     return span;
+//   });
+//   target.querySelector('span').innerText = name;
+// }
 
-function setColor(color) {
-  links.map(item => {
-    let link = item;
-    link.style.color = color;
-    return link
-  })
-}
+// function setColor(color) {
+//   links.map(item => {
+//     let link = item;
+//     link.style.color = color;
+//     return link
+//   })
+// }
