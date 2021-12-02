108894fe204629fde2780a09b53cb547d1bf1521
Vika-star <72336972+Vika-star@users.noreply.github.com>
2021-12-02 16:31:15 +0300

    changing mobil verhion

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,7 +2,7 @@ const homeSection = document.getElementById('bannerSection');
 let homeSectionTop = homeSection.offsetTop;
 
 const aboutSection = document.getElementById('aboutSection');
-let aboutSectionTop = aboutSection.offsetTop - 310;
+let aboutSectionTop = aboutSection.offsetTop - 290;
 
 const servicesSection = document.getElementById('servicesSection');
 let servicesSectionTop = servicesSection.offsetTop - 400;
@@ -13,59 +13,68 @@ let contactSectionTop = contactSection.offsetTop - 300;
 const scrollLines = document.querySelectorAll('.scroll__wrapper-item');
 
 function clearLines() {
+
   scrollLines.forEach(item => {
     item.classList.remove('activeLine');
   })
+
 }
 
 function activateColor(scrollTop) {
 
-  if(scrollTop < aboutSectionTop || scrollTop > contactSectionTop){
+  if (scrollTop < aboutSectionTop || scrollTop > contactSectionTop) {
     document.querySelector('.scroll__wrapper').classList.add('white');
   }
   else {
     document.querySelector('.scroll__wrapper').classList.remove('white');
   }
+
 }
 
 function activateLines(scrollTop) {
-  if(scrollTop >= 0 && scrollTop < aboutSectionTop){
+
+  if (scrollTop >= 0 && scrollTop < aboutSectionTop) {
     clearLines();
     document.querySelector('.scroll__wrapper-item.first').classList.add('activeLine');
   }
-  else if(scrollTop< servicesSectionTop && scrollTop> aboutSectionTop){
+  else if (scrollTop < servicesSectionTop && scrollTop > aboutSectionTop) {
     clearLines();
     document.querySelector('.scroll__wrapper-item.second').classList.add('activeLine');
   }
   else if (scrollTop < contactSectionTop && scrollTop > servicesSectionTop) {
     clearLines();
     document.querySelector('.scroll__wrapper-item.third').classList.add('activeLine');
-  } 
+  }
   else {
     clearLines();
     document.querySelector('.scroll__wrapper-item.fourth').classList.add('activeLine');
   }
+
 }
 
 document.addEventListener("scroll", function () {
   let scrollTop = document.documentElement.scrollTop;
   activateLines(scrollTop);
   activateColor(scrollTop);
-})
+});
 
 
 
 const iconMenu = document.querySelector('.menu__icon');
+const menuLinks = document.getElementsByClassName('menu__link');
+const menuBtn =  document.querySelector('.menu__button');
+
 if (iconMenu) {
   iconMenu.addEventListener("click", closeMobileMenu);
+  menuBtn.addEventListener("click", closeMobileMenu);
+  for (const menuLink of menuLinks) {
+    menuLink.addEventListener("click", closeMobileMenu)
+  }
 }
 
-const menuLinks = document.getElementsByClassName('menu__link');
-for (const menuLink of menuLinks) {
-  menuLink.addEventListener("click", closeMobileMenu)
-}
 
 function closeMobileMenu() {
+
   const bodyMenu = document.querySelector('.menu__body');
   const muneLogo = document.querySelector('.header__logo-img');
 
@@ -73,10 +82,14 @@ function closeMobileMenu() {
   iconMenu.classList.toggle('_active');
   bodyMenu.classList.toggle('_active');
   muneLogo.classList.toggle('_active');
+
 }
 
+
 const btnSend = document.getElementById('sendFrom');
+
 btnSend.addEventListener("click", function () {
+
   const inputEmail = document.getElementById('inputEmail');
   const inputName = document.getElementById('inputName');
 
@@ -92,48 +105,12 @@ btnSend.addEventListener("click", function () {
     for (let i = 0; i < lines.length; i++) {
       lines[i].style.height = "3395px";
     }
+
   }
+
 });
 
 function IsValidInput(input) {
   return input.validity.valid;
 }
 
-
-// const targetsProperties = new Map();
-
-// targetsProperties.set("bannerNavigation", { color: "white", name: "Banner" });
-// targetsProperties.set("aboutUsNavigation", { color: "black", name: "About" });
-// targetsProperties.set("servicesNavigation", { color: "black", name: "Services" });
-// targetsProperties.set("contactNavigation", { color: "white", name: "Contact" });
-
-
-// const navItems = document.getElementsByClassName('nav-item');
-
-// const spans = Array.from(document.getElementsByClassName('navigationName'));
-// let links = Array.from(document.getElementsByClassName('scroll-navigation__li'));
-
-// $(window).on('activate.bs.scrollspy', function (e, obj) {
-
-//   let targetProperties = targetsProperties.get(e.target.id.toString());
-
-//   setName(targetProperties.name, e.target);
-//   setColor(targetProperties.color)
-// });
-
-// function setName(name, target) {
-//   spans.map(item => {
-//     let span = item;
-//     span.innerText = "";
-//     return span;
-//   });
-//   target.querySelector('span').innerText = name;
-// }
-
-// function setColor(color) {
-//   links.map(item => {
-//     let link = item;
-//     link.style.color = color;
-//     return link
-//   })
-// }
